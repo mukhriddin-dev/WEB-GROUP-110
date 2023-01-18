d9e928de78ed3e94ce186d857950ff2041cbfa58
mukhriddin-dev <khodievm@gmail.com>
2023-01-18 16:20:11 +0500

    start module 3 , lesson 1 completed

---
+++ module-3/lesson-1/script.js
@@ -0,0 +1,33 @@
+// console.log("Starting JavaScript");           // consolga xabar chiqarish
+// console.warn('Warning temperal dead zone');   // consolga ogohlantirish xabari
+// console.error(" *** NOT FOUND 404 *** ");     // consolga hatolik xabari
+// console.info('NODE JS ')                      // consolga ma'lumot
+
+// document.write('HELLO WORLD');
+
+// document.getElementById("text").innerHTML="<p>2023.01.18</p>";
+// elementni tanlab ichiga text yoki HTML yozish.
+// document.getElementById("text").textContent="<p>2023.01.18</p>";
+// document.getElementById("text").innerText="<p>2023.01.18</p>";
+
+// document.getElementById('description').innerHTML="lorem lorem lorem ipsum dolor sit amet, consectet";
+
+// alert("lorem lorem lorem ipsum dolor sit amet"); // modal windowda xabar
+// confirm("saytdan chiqasizmi ?") // modal windowda so'rov
+// prompt("ismingiz nima ?")      // modal windowda input  form,
+// prompt("Yoshingiz nechada ?")
+
+// + , _ , / , * , % ,
+
+// ++  INCREMENT
+// --  DECREMENT
+
+//console.log(13%10);
+//console.log(10%13);
+
+//var a=4
+
+//console.log(++a);
+// console.log(a)
+// console.log(--a)
+// console.log(--a)
