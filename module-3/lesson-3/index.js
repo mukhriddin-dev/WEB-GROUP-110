0fec03581803947b0c62cae701b24079de4d4110
mukhriddin-dev <khodievm@gmail.com>
2023-01-23 16:00:58 +0500

    lesson three part 1

---
+++ module-3/lesson-3/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// Math
+// round -> haqqiyiy yahlitlaydi 
+
+let num1 = 9.67;
+
+//   ->   console.log(num1)
+//   ->   console.log(Math.round(num1)) // 10 
+//   ->   console.log(Math.ceil(num1)) // katta tomonga yahlitlaydi
+//   ->   console.log(Math.floor(num1)) // kichik tomonga yahlitlaydi
+//   ->   console.log(Math.abs(num1)) // sonning moduli
+//   ->   console.log(Math.sqrt(num1)) // 3.1096 
+//   ->   console.log(Math.pow(169, 5)) // darajalar 
+//   ->   console.log(Math.max(1, 2, 3, 34, 5, 6, 7, 7, 8, 9, 990)) // min/max
+//   ->   console.log(Math.trunc(num1)) //
+//   ->   console.log(Math.trunc( Math.random() * 100) ) // butun qismini olamiz
+
+// let str = "11abs"*1;
+// let str2=Number(str);
+// console.log(typeof (str2))
+
+
+let num = 222;
+// num += "";
+// console.log(num)
+let num2 = String(num);
+
+console.log(typeof num2)
+
+
+
+
+
+
+
+
